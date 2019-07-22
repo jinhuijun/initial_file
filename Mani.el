05be9ce0e910864ff3d9a2a364d0fd871499e7af
jinhuijun <515602314@qq.com>
2019-07-22 13:12:48 +0800

    Update Mani.el

--- Mani.el
+++ Mani.el
@@ -1,13 +1,14 @@
+;;Mani el V 0.1.3
 (setq org-todo-keywords
-      '((sequence "显化目标：(t)" "反显化目标：(v)" "抉择：(d)" "充能：(c)" "显化：(i)" "反显化：(a)" "|" "已完成：(o)" "已取消：(x@/!)")
-	(sequence " TODO(T)" "-------->WAITING，(W)" "-------->DEFER，(F)" "|" "DONE(D!)" "CANCELED(C@/!)")))
+      '((sequence "显化目标：(t)" "抉择：(d)" "充能：(c)" "行动：(i)" "实相过滤：(f)" "|" "已完成：(o)" "已取消：(x@/!)")
+	(sequence "TODO(T)" "-------->WAITING，(W)" "-------->DEFER，(F)" "|" "DONE(D!)" "CANCELED(C@/!)")))
 
 (setq org-tag-alist
       '((:startgroup . nil)
 	("显化" . ?m) ("反显化" . ?a)
 	(:endgroup . nil)
 	(:startgroup . nil)
-	("想象" . ?i) ("可视化" . ?v) ("情感" . ?e) ("呼请" .?p) ("细节" .?p)
+	("想象" . ?i) ("可视化" . ?v) ("热情奉献" . ?e) ("呼请祈愿" .?p) ("深入细节" . ?d) ("实相塑造" . ?b)
 	(:endgroup . nil)
 	(:startgroup . nil)
 	("执0" . ?0) ("执1" . ?1) ("执2" . ?2) ("执3" . ?3) ("执4" . ?4) ("执5" . ?5) ("激励" . ?6)
